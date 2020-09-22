a2ed87a09666b9275c6e1ab1431aa8932ed8aadd
Will Ulman <will.ulman@gmail.com>
2020-09-22 14:40:19 -0400

    Set up calls to OpenWeather API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export default () => {
+  (async () => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?zip=30308&appid=${API_KEY}`
+    );
+    console.log(data);
+  })();
   return <h1>Weather App</h1>;
 };
