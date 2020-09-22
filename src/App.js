8c320bb9c97a23210ca8d9872bd4d36f210e0b3c
Will Ulman <will.ulman@gmail.com>
2020-09-22 15:25:36 -0400

    Handle request errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-export default () => {
-  (async () => {
-    const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?zip=30308&appid=${API_KEY}`
+const App = () => {
+  const [zip, setZip] = useState("");
+  const [weatherData, setWeatherData] = useState([]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${API_KEY}`
+      );
+      const { weather, name, main, sys } = data;
+      const cityData = {
+        description: weather[0].description,
+        name: name,
+        temperature: main.temp,
+        id: sys.id,
+      };
+      setWeatherData(weatherData.concat(cityData));
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const renderedResults = weatherData.map((city) => {
+    return (
+      <li key={city.id}>
+        {city.name}: {city.description}, {city.temperature} degrees Fahrenheit
+      </li>
     );
-    console.log(data);
-  })();
-  return <h1>Weather App</h1>;
+  });
+
+  return (
+    <Fragment>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Enter Zip Code</label>
+        <input
+          type="text"
+          value={zip}
+          onChange={(e) => setZip(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      <div>{renderedResults}</div>
+    </Fragment>
+  );
 };
+
+export default App;
